12de6ff7a9f44474066262b6300daa4648789082
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-12-04 19:56:20 +0300

    change_db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,3 @@
 import { Sequelize } from "sequelize";
 
-
-export const sequelize = new Sequelize('application_doc', 'postgres', 'admin', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
+export const sequelize = new Sequelize('postgres://valhalla:ifvIAj9ZEpfszrSvJeOK8eTHVu3Rnm6W@dpg-ce6akrda4991ueuo5uf0-a.frankfurt-postgres.render.com/bids')
