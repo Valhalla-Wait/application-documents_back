fea3bafd579dd5c2d6113722b701556ce21d11c1
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-12-05 15:48:01 +0300

    add_sort

--- src/Document/document.controller.ts
+++ src/Document/document.controller.ts
@@ -5,7 +5,7 @@ import { Document } from "./document.model";
 export class DocumentController {
   static async getDocuments (req:Request, res:Response) {
     try {
-        const documents = await Document.findAll()
+        const documents = await Document.findAll({group: 'count_bids'})
         return res.json(documents)
     } catch (e) {
      return res.status(500).json({message: ServerMessages.serverErr}) 
