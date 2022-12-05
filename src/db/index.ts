9fdd6cb2224a4c7398243548a63d0f52ebef2212
Valhalla-Wait <60570103+Valhalla-Wait@users.noreply.github.com>
2022-12-05 19:33:50 +0300

    add_pool

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,5 +2,11 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize('bids', 'valhalla', 'ifvIAj9ZEpfszrSvJeOK8eTHVu3Rnm6W', {
   host: 'dpg-ce6akrda4991ueuo5uf0-a',
-  dialect: 'postgres'
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
